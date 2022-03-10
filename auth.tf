ff9510d7b8894c640fa8eaf5cc9e9182e1962dbf
Nikolai Wotton <nwotton@cmtelematics.com>
2022-03-09 23:51:51 -0500

    terraform

---
+++ auth.tf
@@ -0,0 +1,4 @@
+provider "github" {
+  token = "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"
+  owner = "nikwotton"
+}
---
+++ test.tf
@@ -0,0 +1,5 @@
+resource "github_repository" "generated-repo" {
+  name        = "generated-repo"
+  description = "My generated repository for use with Terraform"
+}
+
